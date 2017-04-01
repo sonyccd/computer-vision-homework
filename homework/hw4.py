43bd1715e60d8473b17dbb89fc5dec46497af573
Brad Bazemore <bwbazemore@uga.edu>
2017-04-01 14:34:37 -0400

    update gauss

--- homework/hw4.py
+++ homework/hw4.py
@@ -4,15 +4,16 @@ import numpy as np
 import scipy.stats as st
 
 
-def gkern(kernlen=21, nsig=3):
-    interval = (2*nsig+1.)/(kernlen)
-    x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
+def gauss_kernel(size=3, sigma=1.0):
+    interval = (2 * sigma + 1.) / size
+    x = np.linspace(-sigma - interval / 2., sigma + interval / 2., size + 1)
     kern1d = np.diff(st.norm.cdf(x))
     kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
-    kernel = kernel_raw/kernel_raw.sum()
+    kernel = kernel_raw / kernel_raw.sum()
     return kernel
+
 
 if __name__ == '__main__':
     image = io.imread('../img/hw1.jpg')
-    plt.imshow(gkern(3, 0.5))
+    plt.imshow(gauss_kernel(3, 0.5))
     plt.show()
