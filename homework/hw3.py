4f3b0109be12e547a976cbab1c75bda55b393ba0
Brad Bazemore <bwbazemore@uga.edu>
2017-04-06 11:07:49 -0400

    added iterative threshold

--- homework/hw3.py
+++ homework/hw3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import skimage.io as io
+import math
 
 
 def _datacheck_peakdetect(x_axis, y_axis):
@@ -132,32 +133,69 @@ def peakdetect(y_axis, x_axis=None, lookahead=200, delta=0):
     return [max_peaks, min_peaks]
 
 
-def peak_detect(histogram, lookahead=5):
-    minum = []
-    maxum = []
-    for i in range(len(histogram)):
+# returns indexes
+def peak_detect(histogram, lookahead=5, min_range=0, max_range=256):
+    minum_index = []
+    maxum_index = []
+    if max_range == 256:
+        max_range = len(histogram)
+    for i in range(min_range, max_range):
         if lookahead < i < len(histogram) - lookahead:
             if histogram[i + 1:i + lookahead].max() < histogram[i] and histogram[i - lookahead:i - 1].max() < histogram[
                 i]:
-                if len(maxum) > 1:
-                    if maxum[len(maxum) - 1] != i - 1:
-                        maxum.append(i)
+                if len(maxum_index) > 1:
+                    if maxum_index[len(maxum_index) - 1] != i - 1:
+                        maxum_index.append(i)
                 else:
-                    maxum.append(i)
+                    maxum_index.append(i)
             if histogram[i + 1:i + lookahead].min() > histogram[i] and histogram[i - lookahead:i - 1].min() > histogram[
                 i]:
-                if len(minum) > 1:
-                    if minum[len(minum) - 1] != i - 1:
-                        minum.append(i)
+                if len(minum_index) > 1:
+                    if minum_index[len(minum_index) - 1] != i - 1:
+                        minum_index.append(i)
                 else:
-                    minum.append(i)
-    return minum, maxum
+                    minum_index.append(i)
+    return minum_index, maxum_index
+
+
+def peakiness(img, dist=20):
+    hist = np.histogram(img.ravel(), 256)[0]
+    maxum_indexs = peak_detect(hist)[1]
+    maxum = np.take(hist, maxum_indexs)
+    print(maxum)
+    min_max_index = np.argpartition(maxum, -4)[-2:]
+    print(maxum_indexs, maxum[min_max_index])
+
+
+def iterative_threshold(img):
+    t = np.mean(img)
+    r1 = img[img > t]
+    r2 = img[img < t]
+    r1_mean_old = np.mean(r1)
+    r2_mean_old = np.mean(r2)
+    while True:
+        t = 0.5 * (r1_mean_old + r2_mean_old)
+        r1 = img[img > t]
+        r2 = img[img < t]
+        r1_mean_new = np.mean(r1)
+        r2_mean_new = np.mean(r2)
+        if math.ceil(r1_mean_old) == math.ceil(r1_mean_new) and math.ceil(r2_mean_old) == math.ceil(r2_mean_new):
+            break
+    return threshold(img, t)
+
+
+# find threshold of image using numpy array masking
+def threshold(img, t):
+    maska = img < t
+    maskb = img >= t
+    img[maska] = 1
+    img[maskb] = 0
+    return img
 
 
 def driver():
     image = io.imread('../img/teeth.jpg')
-    hist = np.histogram(image.ravel(), 256)[0]
-    print(peak_detect(hist))
+    iterative_threshold(image)
 
 
 if __name__ == '__main__':
